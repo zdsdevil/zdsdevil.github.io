89f781d1fe1a2d4e9f41a64d5a8815f451446c66
devil_zds <229026709@qq.com>
2016-08-03 00:15:03 +0800

    。。

--- js/src/resume.js
+++ js/src/resume.js
@@ -2,7 +2,14 @@
  * Created by Administrator on 2016/8/1.
  */
 (function() {
-  $('#fullpage').fullpage();
+  $('#fullpage').fullpage({
+      anchors: ['page1', 'page2', 'page3', 'page4'],
+      afterLoad: function(anchorLink, index) {
+          var _id = '#page' + index;
+          var loadedSection = $(_id);
+          loadedSection.find(".title").addClass("titleAnimate");
+      }
+  });
 
   var myBirth = new Date('1989-09-23');
   var exp = new Date('2014-01-01');
