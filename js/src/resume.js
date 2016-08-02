edec8323d70ba7356bdb9fffe1d5349270d93af2
devil_zds <229026709@qq.com>
2016-08-02 14:16:39 +0800

    aa

--- js/src/resume.js
+++ js/src/resume.js
@@ -5,6 +5,7 @@
   $('#fullpage').fullpage();
 
   $(".play").click(function() {
+    alert(1);
     $("audio").play();
   });
 
@@ -20,32 +21,4 @@
     expText = '';
   }
   $(".exp").text(curTime.getFullYear() - exp.getFullYear() + '年' + expText);
-  var typewriter=document.querySelector(".typewriter"),
-    code=typewriter.innerHTML,
-    pos=0,
-    len=code.length;
-  typewriter.innerHTML="";
-  typewriter.style.display="block";
-  function typewriting(){
-    pos++;
-    if(pos<=len)
-    {
-      switch(code.charAt(pos))
-      {
-        case "<":
-          pos=code.indexOf(">",pos);
-          break;
-        case "&":
-          pos=code.indexOf(";",pos);
-          break;
-      }
-      typewriter.innerHTML=code.substring(0,pos);
-      setTimeout(typewriting,100);
-    }
-    else
-    {
-      typewriter.classList.add("gameover");
-    }
-  }
-  typewriting();
 })();
